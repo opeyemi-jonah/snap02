dd7174ada7e0d0819514f6b7ae28606be15852b2
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2020-03-16 09:54:52 -0600

    Add files via upload

---
+++ snap03.js
@@ -0,0 +1,39 @@
+/* Write an algorithm, either in pseudocode or in code, that converts weights to grams.
+The algorithm should take two arguments.
+The first argument will be a number,
+the second will be an abbreviation for a unit of weight, either "lbs", "oz", "kg", "g", or "mg".
+Based on the second argument, convert the number to grams and output it.
+
+	Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number.
+	(The factors of a number are all numbers that divide evenly into it.
+	For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
+	*/
+
+//Algorithm
+/*
+A number is equal to x
+to convert kilogram(kg) to grams: kg/1000
+Therefore, x(g)=x(kg)/1000.
+
+to convert lbs to gram:
+ */
+
+function uniquePositiveFactors(number1){
+
+	sum = 0;
+
+	for (i=1;i<=number1;i++){
+
+		//Get factors
+		if(number1%i==0){
+			console.log(i);
+
+			sum = sum + i;
+
+		}
+
+	}
+	console.log(sum)
+}
+
+let result = uniquePositiveFactors(12);
